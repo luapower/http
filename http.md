fb6d3923ad9abc7f411a5aae84e8bd2c314b30fb
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2019-11-18 09:07:01 +0200

    unimportant

--- http.md
+++ http.md
@@ -5,8 +5,8 @@ HTTP 1.1 client & server protocol in Lua.
 
 Works on an abstract I/O API made of three functions:
 
-	* `http:read(buf, maxsz) -> sz | nil,err`
-	* `http:send(s | buf,sz) -> true | nil,'closed'|err`
+	* `http:read(buf, maxsz) -> sz | nil,'closed'|err`
+	* `http:send(s | buf,sz) -> true | nil,err`
 	* `http:close()`
 
 GZip (de)compression can be enabled with `http.zlib = require'zlib'`.
@@ -17,40 +17,41 @@ GZip (de)compression can be enabled with `http.zlib = require'zlib'`.
 
 ## API
 
-`http:send_request(t) -> should_close`
+`http:new(t)
+
+## Client-side API
+
+`http:perform_request(t, write_content) -> http_ver, status, headers, content, closed`
 
 --------------------------------- --------------------------------------------
-Client sets request headers:      Based on fields:
+Client sets request headers:      Based on:
 --------------------------------- --------------------------------------------
-	host                           t.host, t.port
-	connection: close              t.close
-	content-length                 t.content
-	transfer-encoding              t.content
-	accept-encoding                self.zlib
-	content-encoding               t.compress
---------------------------------- --------------------------------------------
-
-`http:read_response(method, write_content, should_close)
-	-> http_ver, status, headers, content, closed`
-
+host                              t.host, t.port
+connection: close                 t.close == true
+content-length                    t.content has length or t.content_size given
+transfer-encoding: chunked        type(t.content) == 'function'
+accept-encoding                   self.zlib
+content-encoding                  t.compress == true|'gzip'|'deflate'
 --------------------------------- --------------------------------------------
 Client reads from response:       In order to:
 --------------------------------- --------------------------------------------
-	status, method                 decide whether to read the body or not.
-	transfer-encoding              read the body in chunks.
-	content-encoding               decompress the body.
-	content-length                 know how much to read from the socket.
-	connection                     read the body in absence of content-length.
+status, method                    decide whether to read the body or not.
+transfer-encoding                 read the body in chunks.
+content-encoding                  decompress the body.
+content-length                    know how much to read from the socket.
+connection                        read the body in absence of content-length.
 --------------------------------- --------------------------------------------
+
+## Server-side API
 
 `http:read_request(write_content) -> http_ver, method, uri, headers, content`
 
 --------------------------------- --------------------------------------------
 Server reads request headers:     In order to:
 --------------------------------- --------------------------------------------
-	transfer-encoding              read the body in chunks.
-	content-encoding               decompress the body.
-	content-length                 know how much to read from the socket.
+transfer-encoding                 read the body in chunks.
+content-encoding                  decompress the body.
+content-length                    know how much to read from the socket.
 --------------------------------- --------------------------------------------
 
 `http:send_response(t, request_headers)`
@@ -58,8 +59,8 @@ Server reads request headers:     In order to:
 --------------------------------- --------------------------------------------
 Server sets response headers:     Based on:
 --------------------------------- --------------------------------------------
-	connection: close              t.close.
-	content-length                 t.content_size or t.content's length.
-	transfer-encoding: chunked     if t.content is a reader function.
-	content-encoding: gzip|deflate t.compress, self.zlib, accept-encoding header.
+connection: close                 t.close.
+content-length                    t.content_size or t.content's length.
+transfer-encoding: chunked        if t.content is a reader function.
+content-encoding: gzip|deflate    t.compress, self.zlib, accept-encoding header.
 --------------------------------- --------------------------------------------
--- http_headers.lua
+++ http_headers.lua
@@ -1,5 +1,5 @@
 
---http header values parsing.
+--http header values parsing and formatting.
 --Written by Cosmin Apreutesei. Public Domain.
 
 if not ... then require'http_headers_test'; return end
@@ -8,6 +8,9 @@ local glue = require'glue'
 local b64 = require'libb64'
 local http_date = require'http_date'
 local re = require'lpeg.re' --for tokens()
+local uri = require'uri'
+
+local headers = {}
 
 --simple value parsers
 
@@ -560,34 +563,149 @@ parse.x_powered_by = glue.pass  --PHP/5.2.1
 
 --parsing API
 
-local function parse_value(k,v)
+function headers.parse(k, v)
+	local k = k:gsub('-', '_')
 	if parse[k] then return parse[k](v) end
 	return v --unknown header, return unparsed
 end
 
-local function parse_values(t)
-	local dt, errt = {}, {}
-	for k,v in pairs(t) do
-		local pv,err = parse_value(k,v)
-      dt[k] = pv
-		if pv == nil then
-			errt[k] = err or string.format('invalid value "%s"', v)
-		end
-	end
-	return dt, errt
-end
-
-local function lazy_parse(t) --lazy_parse(s) -> t; t.header_name -> parsed_value
-	return setmetatable({}, {__index = function(dt,k)
-		dt[k] = parse_value(k, t[k])
+--lazy parse headers: headers.parsed(t) -> t; t.header_name -> parsed_value
+function headers.parsed(t)
+	return setmetatable({}, {__index = function(dt, k)
+		dt[k] = headers.parse(k, t[k])
 		return rawget(dt,k)
 	end})
 end
 
-return {
-	parsers = parse,
-	parse_value = parse_value,
-	parse_values = parse_values,
-	lazy_parse = lazy_parse,
+--formatting -----------------------------------------------------------------
+
+local ci = string.lower
+local base64 = b64.encode_string
+
+local function int(v)
+	glue.assert(math.floor(v) == v, 'integer expected')
+	return tostring(v)
+end
+
+--{v1,v2,...} -> v1,v2,...
+local function list(t, format)
+	format = format or glue.pass
+	local dt = {}
+	for i,v in ipairs(t) do
+		dt[#dt+1] = format(v)
+	end
+	return table.concat(dt, ',')
+end
+
+local function cilist(s)
+	return format_list(s, string.lower)
+end
+
+--{k1=v1,...} -> k1=v1,...
+local function kv_list(kvt)
+	local t = {}
+	for k,v in sorted_pairs(kvt) do
+		if v then
+			t[#t+1] = v == true and k or string.format('%s=%s', k, v)
+		end
+	end
+	return table.concat(t, ',')
+end
+
+--{name,k1=v1,...} -> name[; k1=v1 ...]
+local function params(known)
+	return function(s)
+	end
+end
+
+--{from=,to=,size=} -> bytes=<from>-<to>
+local function request_range(v)
+end
+
+--{from=,to=,total=,size=} -> bytes <from>-<to>/<total>
+local function response_range(v)
+end
+
+
+local function cookies(cookies)
+end
+
+local nofold_headers = { --headers that it isn't safe to send them folded
+	set_cookie = true,
+	cookie = true,
+	www_authenticate = true,
 }
 
+local formatters = {
+	--general header fields
+	cache_control = kv_list, --no_cache
+	connection = ci,
+	content_length = int,
+	content_md5 = base64,
+	content_type = params{charset = ci}, --text/html; charset=iso-8859-1
+	date = date,
+	pragma = nil, --cilist?
+	trailer = headernames,
+	transfer_encoding = cilist,
+	upgrade = nil, --http/2.0, shttp/1.3, irc/6.9, rta/x11
+	via = nil, --1.0 fred, 1.1 nowhere.com (apache/1.1)
+	warning = nil, --list of '(%d%d%d) (.-) (.-) ?(.*)' --code agent text[ date]
+	--standard request headers
+	accept = cilist, --paramslist?
+	accept_charset = cilist,
+	accept_encoding = cilist,
+	accept_language = cilist,
+	authorization = ci, --basic <password>
+	cookie = kv, --TODO: kv';',
+	expect = cilist, --100-continue
+	from = nil, --user@example.com
+	host = nil,
+	if_match = nil, --<etag>
+	if_modified_since = date,
+	if_none_match = nil, --etag
+	if_range = nil, --etag
+	if_unmodified_since	= date,
+	max_forwards = int,
+	proxy_authorization = nil, --basic <password>
+	range = request_range, --bytes=500_999
+	referer = nil, --it's an url but why parse it
+	te = cilist, --"trailers, deflate"
+	user_agent = nil, --mozilla/5.0 (compatible; msie 9.0; windows nt 6.1; wow64; trident/5.0)
+	--non-standard request headers
+	x_requested_with = ci,--xmlhttprequest
+	dnt = function(v) return v[#v]=='1' end, --means "do not track"
+	x_forwarded_for = nil, --client1, proxy1, proxy2
+	--standard response headers
+	accept_ranges = ci, --"bytes"
+	age = int, --seconds
+	allow = cilist, --method
+	content_disposition = params{filename = nil}, --attachment; ...
+	content_encoding = ci,
+	content_language = cilist,
+	content_location = url,
+	content_range = response_range, --bytes 0-500/1250
+	etag = nil,
+	expires = date,
+	last_modified = date,
+	link = nil, --?
+	location = url,
+	p3p = nil,
+	proxy_authenticate = ci, --basic
+	refresh = params{url = url}, --seconds; ... (not standard but supported)
+	retry_after = int, --seconds
+	server = nil,
+	set_cookie = cookies,
+	strict_transport_security = nil, --eg. max_age=16070400; includesubdomains
+	vary = headernames,
+	www_authenticate = ci,
+	--non-standard response headers
+	x_Forwarded_proto = ci, --https|http
+	x_powered_by = nil, --PHP/5.2.1
+}
+
+function headers.format(k, v)
+	local k = k:gsub('-', '_')
+	return formatters[k] and formatters[k](v) or v
+end
+
+return headers
